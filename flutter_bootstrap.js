efb534a8fbcf98e1dfc347345390a2be3be0e04d
SYA <sya@noreply.invalid>
2026-09-14 22:50:02 +0900

    웹: 초대 링크(join.html · ?join=) · 코드 예시 K7M2-QRTV

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3788523251" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3732632361" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
