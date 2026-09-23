498b4a53c79b4eb47b9f75a353046e68c7c4e17a
SYA <sya@noreply.invalid>
2026-09-23 15:00:32 +0900

    첫 화면 예시 안내·예시 전화번호 (hakwon-app cddf413)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "846802049" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1786413464" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
